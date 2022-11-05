137d8bda576065c4d3d150e1f68ef366c53ebaa6
ishmyles <33308871+ishmyles@users.noreply.github.com>
2022-11-05 06:26:19 +0000

    Added mongoose package w/ working connection to DB

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+mongoose.connect(process.env.DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "))
+db.once("open", () => console.log("Connected successfully"))
+
 app.use(cors())
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
